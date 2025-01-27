24cc0b8194f3b9bb0b21edaed9b0e668e6a2517a
Maxence Isaac Castiau <misaac-c@c2r5s6.42madrid.com>
2025-01-27 16:58:44 +0100

    with norm

--- src/signals/signal.c
+++ src/signals/signal.c
@@ -6,11 +6,12 @@
 /*   By: misaac-c <misaac-c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 20:02:20 by misaac-c          #+#    #+#             */
-/*   Updated: 2025/01/27 16:56:47 by misaac-c         ###   ########.fr       */
+/*   Updated: 2025/01/27 16:58:25 by misaac-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
 int	g_signal = 0;
 
 void	sigint_handler(int sig)
@@ -27,7 +28,7 @@ void	manage_signals(t_shell *shell)
 {
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, &sigint_handler);
-	if(g_signal == 1)
+	if (g_signal == 1)
 	{
 		shell->last_exit_status = 130;
 		g_signal = 0;
